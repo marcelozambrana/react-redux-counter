5e9c8583de7656a7b0b7a7723fb705f4eb88a3f8
Marcelo Zambrana de Macedo <marcelo@eurekalabs.com.br>
2018-06-21 20:15:42 -0300

    react redux counter

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Counter from './Counter'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import counterReducer from './reducer'
+
+let store = createStore(counterReducer)
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+      </Provider>
+    )
+  }
+}
+
+export default App
